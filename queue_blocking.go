0322a619d5c665a9413c47261402904434454181
CC11001100 <CC11001100@qq.com>
2022-10-29 22:09:37 +0800

    x

--- queue_blocking.go
+++ queue_blocking.go
@@ -2,8 +2,18 @@ package queue
 
 import "context"
 
+// BlockingQueue 阻塞队列所拥有的的方法的定义
 type BlockingQueue[T any] interface {
 	Queue[T]
-	BPut(ctx context.Context, value T) (err error)
-	BTake(ctx context.Context) (value T, err error)
+
+	BPut(ctx context.Context, value T) error
+
+	BTake(ctx context.Context) T
+	BTakeE(ctx context.Context) (T, error)
+
+	// BPeek 取出队列头部的元素，如果队列为空会返回对应类型的零值
+	BPeek(ctx context.Context) T
+
+	// BPeekE 取出队列头部的元素，如果队列为空会返回error
+	BPeekE(ctx context.Context) (T, error)
 }
--- queue_linked.go
+++ queue_linked.go
@@ -1,59 +1,19 @@
 package queue
 
-// 基于链表实现的队列
-
+// LinkedQueue 基于链表实现的队列
 type LinkedQueue[T any] struct {
-	head *Node[T]
-	tail *Node[T]
+
+	// 队列的第一个元素
+	head *LinkedListNode[T]
+
+	// 队列的最后一个元素
+	tail *LinkedListNode[T]
 }
 
 var _ Queue[any] = &LinkedQueue[any]{}
 
 func NewLinkedQueue[T any]() *LinkedQueue[T] {
 	return &LinkedQueue[T]{}
-}
-
-func (x *LinkedQueue[T]) Put(values ...T) (err error) {
-	for _, value := range values {
-		node := &Node[T]{
-			value: value,
-		}
-		// 此时链表为空
-		if x.tail == nil {
-			x.head = node
-			x.tail = node
-		} else {
-			// 链表不为空，追加到最后即可
-			x.tail.next = node
-		}
-	}
-	return nil
-}
-
-func (x *LinkedQueue[T]) Take() (value T, err error) {
-	if x.head == nil {
-		return nil, err
-	}
-	head := x.head
-
-	// 链表中只有一个元素，还被取走了，此时需要将链表置为空
-	if x.head == x.tail {
-		x.tail = nil
-		x.head = nil
-	} else {
-		x.head = x.head.next
-	}
-	head.next = nil
-	value = head.value
-	head.value = nil
-	return
-}
-
-func (x *LinkedQueue[T]) Peek() (value T, err error) {
-	if x.head == nil {
-		return nil, nil
-	}
-	return x.head.value, nil
 }
 
 func (x *LinkedQueue[T]) IsEmpty() bool {
@@ -64,6 +24,7 @@ func (x *LinkedQueue[T]) IsNotEmpty() bool {
 	return x.head != nil
 }
 
+// Len 注意这个方法的时间复杂度是O(n)
 func (x *LinkedQueue[T]) Len() int {
 	count := 0
 	currentNode := x.head
@@ -79,6 +40,69 @@ func (x *LinkedQueue[T]) Clear() error {
 	return nil
 }
 
+func (x *LinkedQueue[T]) Put(values ...T) error {
+	for _, value := range values {
+		node := &LinkedListNode[T]{
+			value: value,
+		}
+		// 此时链表为空
+		if x.tail == nil {
+			x.head = node
+			x.tail = node
+		} else {
+			// 链表不为空，追加到最后一个节点后面即可
+			x.tail.next = node
+			// 然后再把最后一个节点指向自己
+			x.tail = node
+		}
+	}
+	return nil
+}
+
+func (x *LinkedQueue[T]) Take() T {
+	e, err := x.TakeE()
+	if err != nil {
+		var zero T
+		return zero
+	}
+	return e
+}
+
+func (x *LinkedQueue[T]) TakeE() (T, error) {
+	if x.head != nil {
+		return nil, ErrQueueEmpty
+	}
+	nowHeadNode := x.head
+
+	// 队列中只有一个元素，首和尾指向的是同一个元素
+	if x.head == x.tail {
+		x.head = nil
+		x.tail = nil
+	} else {
+		// 队列中至少有两个元素，把头结点直接往后移动就可以了
+		x.head = x.head.next
+		nowHeadNode.next = nil
+	}
+
+	return nowHeadNode.value, nil
+}
+
+func (x *LinkedQueue[T]) Peek() T {
+	e, err := x.PeekE()
+	if err != nil {
+		var zero T
+		return zero
+	}
+	return e
+}
+
+func (x *LinkedQueue[T]) PeekE() (T, error) {
+	if x.head == nil {
+		return nil, ErrQueueEmpty
+	}
+	return x.head.value, nil
+}
+
 func (x *LinkedQueue[T]) String() string {
 	// TODO 2022-10-22 00:07:58
 	return ""
@@ -86,7 +110,7 @@ func (x *LinkedQueue[T]) String() string {
 
 // ------------------------------------------------ ---------------------------------------------------------------------
 
-type Node[T any] struct {
+type LinkedListNode[T any] struct {
 	value T
-	next  *Node[T]
+	next  *LinkedListNode[T]
 }
